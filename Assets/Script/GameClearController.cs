130159b67d9fc94ad8f887d4e04f3de2f666a394
MatumotoTomoki <ca01254019@st.kawahara.ac.jp>
2025-08-06 12:30:52 +0900

    ゲームクリア画面の追加 (#14)

---
+++ Assets/Script/GameClearController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameClearController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onClickStarButton()
+    {
+        SceneManager.LoadScene("Title");
+    }
+}
