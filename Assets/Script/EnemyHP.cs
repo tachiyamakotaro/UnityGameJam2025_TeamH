23f269095f738258e6102ec39dca779d4d9e86b3
Kawaharatarou <Ca01254015tonokouti>
2025-08-06 11:09:07 +0900

    Enemyの移動

--- Assets/Script/EnemyHP.cs
+++ Assets/Script/EnemyHP.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class EnemyHP : MonoBehaviour
 {
     private int hp = 3;
+    private MeshRenderer[] meshRenderers;
+    private Collider[] colliders;
     // Start is called before the first frame update
     void Start()
     {
-        
+        // 自身と子オブジェクトすべての MeshRenderer を取得
+        meshRenderers = GetComponentsInChildren<MeshRenderer>();
+        // コライダーも無効にする用
+        colliders = GetComponentsInChildren<Collider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -28,4 +33,26 @@ public class EnemyHP : MonoBehaviour
             Destroy(other.gameObject);
         }
     }
+    private void OnDeath()
+    {
+        // メッシュを非表示にする
+        foreach (var mr in meshRenderers)
+        {
+            mr.enabled = false;
+        }
+
+        // コライダーも無効化
+        foreach (var col in colliders)
+        {
+            col.enabled = false;
+        }
+
+        // 必要があればスクリプトや動きを止める処理も追加
+        // 例: this.enabled = false;
+    }
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+   
 }
